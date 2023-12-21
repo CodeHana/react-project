f1a8a26f04641c0c9cb1baf3d4689bcc9372c682
Huang <cindy.psu.h@gmail.com>
2023-12-20 17:40:03 -0800

    init project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import GameCard from "./GameCard";
+const gamesList = [
+  {
+    id: 0,
+    name: "Scythe",
+    rating: null, 
+    category: "Fun"
+  },
+  {
+    id: 1,
+    name: "Machi Koro",
+    rating: null, 
+    category: "Deck Building"
+  },
+  {
+    id: 2,
+    name: "Galacy Trucker",
+    rating: null, 
+    category: "Fun"
+  }
+]
+
+function App() {
+  return (
+    <div>
+      <h1>My Board Games</h1>
+      {gamesList.map( g => ( <GameCard game={g}/> ))}
+    </div>
+  );
+}
+
+export default App;
